8d6bfe814e6430a43c422d074c8a2405a2bc8936
mohamed_sarrou <mohamedserrou2016@gmail.com>
2022-08-31 16:18:33 +0100

    linkcrud

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Agent;
+use App\Entity\Client;
+use App\Entity\Fourgon;
+use App\Entity\Fournisseur;
+use App\Entity\Parking;
+use App\Entity\Remorque;
+use App\Entity\Transaction;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,6 +34,12 @@ class AdminDashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Gestion Agent', 'fas fa-list', Agent::class);
+        yield MenuItem::linkToCrud('Clients', 'fas fa-list', Client::class);
+        yield MenuItem::linkToCrud('Fournisseur', 'fas fa-list', Fournisseur::class);
+        yield MenuItem::linkToCrud('Fourgon', 'fas fa-list', Fourgon::class);
+        yield MenuItem::linkToCrud('Remorque', 'fas fa-list', Remorque::class);
+        yield MenuItem::linkToCrud('Parking', 'fas fa-list', Parking::class);
+        yield MenuItem::linkToCrud('Transit', 'fas fa-list', Transaction::class);
     }
 }
