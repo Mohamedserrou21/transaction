334adde95dcb70b55b93c566e2bdd0d035f049b0
mohamed_sarrou <mohamedserrou2016@gmail.com>
2022-08-31 16:27:16 +0100

    transitcrud

--- src/Controller/Admin/TransactionCrudController.php
+++ src/Controller/Admin/TransactionCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Transaction;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use phpDocumentor\Reflection\Types\Integer;
 
 class TransactionCrudController extends AbstractCrudController
 {
@@ -12,14 +17,22 @@ class TransactionCrudController extends AbstractCrudController
         return Transaction::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+
+            DateField::new('Date_transit'),
+            IntegerField::new('Montant'),
+            TextField::new('Num_facture'),
+            TextField::new('Num_dossier'),
+            AssociationField::new('Agent_matricule'),
+            AssociationField::new('Fournisseur_matri'),
+            AssociationField::new('Client'),
+            AssociationField::new('fourgon'),
+            AssociationField::new('Remorque'),
+            AssociationField::new('Parking_matricule'),
+
         ];
     }
-    */
 }
